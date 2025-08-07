16d0d3be67a81f168df4ae7f387b6f8906f7ca62
Serhii <tustanovskiis@gmail.com>
2025-08-07 22:40:14 +0200

    feat: added update, delete, get by id

--- app/infrastructure/repository.py
+++ app/infrastructure/repository.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from infrastructure.model import Todo
-from infrastructure.schema import TodoCreate, TodoUodate, TodoRead
+from infrastructure.schema import TodoCreate, TodoUpdate, TodoRead
 
 
 def createTodo(db: Session, todo: TodoCreate) -> Todo:
@@ -12,3 +12,26 @@ def createTodo(db: Session, todo: TodoCreate) -> Todo:
 
 def getAll(db: Session, skip: int = 0, limit: int = 10) -> Todo | None:
   return db.query(Todo).offset(skip).limit(limit).all()
+
+def getOne(db: Session, todo_id: int) -> Todo | None:
+    return db.query(Todo).filter(Todo.id == todo_id).first()
+
+
+def updateTodo(db: Session, todo_id: int, data: TodoUpdate) -> Todo | None:
+    db_todo = getOne(db, todo_id)
+    if not db_todo:
+        return None
+    for field, value in data.model_dump(exclude_unset=True).items():
+        setattr(db_todo, field, value)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+
+def deleteTodo(db: Session, todo_id: int) -> bool:
+    db_todo = getOne(db, todo_id)
+    if not db_todo:
+        return False
+    db.delete(db_todo)
+    db.commit()
+    return True
--- app/presentation/handler.py
+++ app/presentation/handler.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import Session
 
 from database import SessionLocal
 from infrastructure.schema import TodoRead
-from presentation.schema import TodoCreate, Todo
-from infrastructure import repository as repository
+from presentation.schema import TodoCreate, TodoUpdate
+from infrastructure import repository
 
 router = APIRouter(prefix="/todos", tags=["Todos"])
 
@@ -24,3 +24,24 @@ def create(todo: TodoCreate, db: Session = Depends(getDB)):
 @router.get("/", response_model=list[TodoRead])
 def getAll(skip: int = 0, limit: int = 10, db: Session = Depends(getDB)):
   return repository.getAll(db, skip=skip, limit=limit)
+
+
+@router.get("/{todo_id}", response_model=TodoRead)
+def read(todo_id: int, db: Session = Depends(getDB)):
+    db_todo = repository.getOne(db, todo_id)
+    if not db_todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
+
+@router.patch("/{todo_id}", response_model=TodoRead)
+def update(todo_id: int, todo: TodoUpdate, db: Session = Depends(getDB)):
+    db_todo = repository.updateTodo(db, todo_id, todo)
+    if not db_todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
+
+@router.delete("/{todo_id}", status_code=204)
+def delete(todo_id: int, db: Session = Depends(getDB)):
+    success = repository.deleteTodo(db, todo_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Todo not found")
